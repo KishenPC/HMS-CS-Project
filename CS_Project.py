2a13449784bd201b609d7fb10ba75c0305afe5b7
KishenPC <138299991+KishenPC@users.noreply.github.com>
2023-10-23 12:25:49 +0530

    Updation of Project Code
    Added Connection Try/Except For main()
             Done to catch exceptions during connection
    Added Connection Credentials Input 
             Simply cuz cool and that
    Added A Show Tables Function to show all tables in existing Database

--- CS_Project.py
+++ CS_Project.py
@@ -1,7 +1,36 @@
+def show_tables():
+    conn_cursor = conn.cursor()
+    conn_cursor.execute("SHOW TABLES")
+    show_tables = conn_cursor.fetchall()
+    tables = []
+    for i in show_tables:
+        for z in i: #Due to the fact that returned list of tables in the form [(t1,),(t2,)]
+            tables.append(z)
+    for i in tables:
+        print(f"{tables.index(i)+1} - {i}")
+
 import mysql.connector
+from mysql.connector import Error
+from mysql.connector import errorcode
+Datalist = []
 
-conn = mysql.connector.connect(host="localhost", user="root", database="", passwd="")
-cur = conn.cursor()
+try:
+    print("          Enter SQL Server Connection Details")
+    print("__________________________________________________________ ")
+    host = input("Host Name: ")
+    user = input("User Name: ")
+    Database = input("Database Name: ")
+    passwd = input("Database Connection Password: ")
+    conn = mysql.connector.connect(host=host, user=user , database=Database, passwd=passwd)
+    if conn.is_connected():
+        print("Database Connection Established!\n")
+    print(f"-✧˖°. Welcome To The {Database} Databse Interface ˖°.✧-\n")
+    print("List of Tables-")
+    show_tables()
+    
+    conn.close()
 
-conn.commit()
-conn.close()
+except mysql.connector.Error:
+    print("Connection Failed!")
+    print(mysql.connector.Error)
+    print("Make sure you have entered the right credentials for the database connection")
