03ef292363ea849291f9bfb60b8d2cfa0b990857
AbinKrishna57 <127468485+AbinKrishna57@users.noreply.github.com>
2023-10-23 16:56:03 +0530

    Update CS_Project.py

--- CS_Project.py
+++ CS_Project.py
@@ -1,11 +1,10 @@
-import mysql.connector
-from mysql.connector import Error
-from mysql.connector import errorcode
+import mysql.connector as sq
+from getpass import getpass
 
 def show_tables():
-    conn_cursor = conn.cursor()
-    conn_cursor.execute("SHOW TABLES")
-    show_tables = conn_cursor.fetchall()
+    cursor = conn.cursor()
+    cursor.execute("SHOW TABLES")
+    show_tables = cursor.fetchall()
     tables = []
     for i in show_tables:
         for z in i: #Due to the fact that returned list of tables in the form [(t1,),(t2,)]
@@ -16,22 +15,25 @@ def show_tables():
 Datalist = []
 
 try:
-    print("          Enter SQL Server Connection Details")
-    print("__________________________________________________________ ")
+    print("\n\t\t\tEnter SQL Server Connection Details")
+    print("_"*91)
+
     host = input("Host Name: ")
     user = input("User Name: ")
-    Database = input("Database Name: ")
-    passwd = input("Database Connection Password: ")
-    conn = mysql.connector.connect(host=host, user=user , database=Database, passwd=passwd)
+    db = input("Database Name: ")
+    passwd = getpass("Database Connection Password: ") # By using "getpass" whenever u type the password it will be hidden in the terminal
+    
+    conn = sq.connect(host=host, username=user , database=db, passwd=passwd)
+
     if conn.is_connected():
-        print("Database Connection Established!\n")
-    print(f"-✧˖°. Welcome To The {Database} Databse Interface ˖°.✧-\n")
+        print("\nDatabase Connection Established!\n")
+    print(f"-✧˖°. Welcome To The {db} Databse Interface ˖°.✧-\n")
     print("List of Tables-")
     show_tables()
     
     conn.close()
 
-except mysql.connector.Error:
-    print("Connection Failed!")
-    print(mysql.connector.Error)
-    print("Make sure you have entered the right credentials for the database connection")
+except sq.Error:
+    print("\nConnection Failed!")
+    print(sq.Error)
+    print("Make sure you have entered the right credentials for the database connection\n")
