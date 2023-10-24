2784f4d7e12f843b1acacecfc3b1049a5c281966
AbinKrishna57 <127468485+AbinKrishna57@users.noreply.github.com>
2023-10-24 15:49:00 +0530

    Update CS_Project.py

--- CS_Project.py
+++ CS_Project.py
@@ -1,51 +1,69 @@
+# Imports
+import mysql.connector as sq
+from getpass import getpass
+
+# [!] - for errors
+# [⁎] - for goods (i dont know what to write here)
+# [#] - if something is not available or not found
+
+# Functions
 def describe_table():
-    conn_cursor = conn.cursor()
-    table = input("Enter Name Of Table To Describe: ")
-    conn_cursor.execute(f"DESCRIBE {table}")
-    table_desc = conn_cursor.fetchall()
+    cursor = conn.cursor()
+    table = input("(Enter Name Of Table To Describe) > ")
+    cursor.execute(f"DESCRIBE {table}")
+    table_desc = cursor.fetchall()
     columns = {} 
-    print("\nThe Format Is, {Column Name : Data Type}")
+    print("\n[⁎] The Format Is, {Column Name : Data Type}")
     for i in table_desc:
         columns.update({i[0]:i[1].upper()})
     print(f"{columns}\n")
 
 def show_tables():
-    conn_cursor = conn.cursor()
-    conn_cursor.execute("SHOW TABLES")
-    show_tables = conn_cursor.fetchall()
+    cursor = conn.cursor()
+    cursor.execute("SHOW TABLES")
+    show_tables = cursor.fetchall()
     tables = []
     for i in show_tables:
         for z in i: #Due to the fact that returned list of tables in the form [(t1,),(t2,)]
             tables.append(z)
+    
     if len(tables) == 0:
-        print("There Are No Available Tables in Your Database") 
+        print("[#] There Are No Available Tables in Your Database") 
     else:
         print("\t\t.___________________.")
         print("\t\tList of Tables\n")
         for i in tables:
             print(f"\t\t{tables.index(i)+1}| {i}")
         print("\t\t.___________________.")
-    conn.commit()
 
 def insert_values():
-    conn_cursor = conn.cursor()
-    table = input("Enter Table Name: ")
-    entries = int(input("Enter Number Of Entries: "))
-    print("The Columns Of The Table is as Follows: ")
-    conn_cursor.execute(f"DESCRIBE {table}")
-    table_desc = conn_cursor.fetchall()
+    cursor = conn.cursor()
+    table = input("(Enter Table Name) > ")
+    entries = int(input("(Enter Number Of Entries) > "))
+
+    print("[⁎] The Columns Of The Table is as Follows: ")
+    cursor.execute(f"DESCRIBE {table}")
+    table_desc = cursor.fetchall()
     columns = {} 
     for i in table_desc:
         columns.update({i[0]:i[1].upper()})
-    print(f"{columns}\n")
-    print("Enter The Values In Order Seperated By / ~ [A,B,C,1,2]")
-    for i in range(entries):
-        entry = tuple(input().split("/"))
-        conn_cursor.execute(f"INSERT INTO {table} VALUES {entry}") #Under Maintenance
-    conn.commit()
-    
+    print(columns)
 
-        
+    ent=[]
+    for i in range(entries):
+        entry = tuple(input("(Enter values) > ").split())
+        ent.append(entry)
+    cursor.executemany("INSERT INTO "+table+" VALUES(%s, %s, %s, %s, %s, %s)", ent)
+    # %s is the no. of columns (you can change it according to the no. of columns)
+
+    
+    # this is just for the memes
+    cursor.execute("SELECT * FROM " + table)
+    item=cursor.fetchall()
+    for a in item:
+        print(a)
+    ############################
+
 def create_hosptial_db():
     conn_cursor = conn.cursor()
     conn_cursor.execute("SHOW TABLES")
@@ -57,46 +75,75 @@ def create_hosptial_db():
     if ('Patient', 'Doctor') in tables:
         print()
     else:
-        conn_cursor.execute("CREATE TABLE Patient(Patient_ID INT PRIMARY KEY, Patient_Name VARCHAR(30), Patient_Gender VARCHAR(20), Address VARCHAR(50), Phone INT, Insurance_ID INT)")
-        conn_cursor.execute("CREATE TABLE Doctor(Doctor_ID INT PRIMARY KEY, Doctor_Name VARCHAR(30), Specialization VARCHAR(30), Doctor_Gender VARCHAR(20), Address VARCHAR(50), Phone INT)") 
+        conn_cursor.execute("""CREATE TABLE Patient(
+                            Patient_ID INT PRIMARY KEY,
+                            Patient_Name VARCHAR(30),
+                            Patient_Gender VARCHAR(20),
+                            Address VARCHAR(50),
+                            Phone INT,
+                            Insurance_ID INT)""")
         
+        conn_cursor.execute("""CREATE TABLE Doctor(
+                            Doctor_ID INT PRIMARY KEY,
+                            Doctor_Name VARCHAR(30),
+                            Specialization VARCHAR(30),
+                            Doctor_Gender VARCHAR(20),
+                            Address VARCHAR(50),
+                            Phone INT)""") 
 
-import mysql.connector as sq
-from mysql.connector import Error
-from mysql.connector import errorcode
+# Code
+print("\n"+"="*80)
+print("  [Project] Hospital Management System")
+print("="*80)
+
+print("  [Credits] Abin Krishna, Kishen PC, Vaishak")
+print("="*80+"""
+
+  ____             _    _        ____                      
+ |  _ \ ___   ___ | | _(_) ___  | __ )  ___  __ _ _ __ ____
+ | |_) / _ \ / _ \| |/ / |/ _ \ |  _ \ / _ \/ _` | '__|_  /
+ |  __/ (_) | (_) |   <| |  __/ | |_) |  __/ (_| | |   / / 
+ |_|   \___/ \___/|_|\_\_|\___| |____/ \___|\__,_|_|  /___|
+                                                           
+""")
+
 
 try:
-    print("\n\t\t\tEnter SQL Server Connection Details")
+    print("\n\t\t\tEnter SQL Server Connection Details\n")
     print("_"*91)
-    host = input("Host Name: ")
-    user = input("User Name: ")
-    db = input("Database Name: ")
-    passwd = input("Database Connection Password: ")
-    conn = sq.connect(host=host, user=user , database=db, passwd=passwd)
+
+    host = input("\n(Hostname) > ")
+    user = input("(Username) > ")
+    db = input("(Database Name) > ")
+    passwd = getpass("(Database Connection Password) > ")
+    # By using "getpass" whenever u type the password it will be hidden in the terminal
+    
+    conn = sq.connect(host=host, username=user , database=db, passwd=passwd)
+    
     if conn.is_connected():
-        print("\n✓ Database Connection Established! ✓")
-        print("_"*91)
-        print(f"\n-✧˖° Welcome To The HMS Databse Interface ˖°.✧-")
-        create_hosptial_db()
+        print(f"\n[⁎] Connected to {host}")
+        print(f"[⁎] Welcome To The {db} Databse Interface\n")
+        print("[⁎] List of Tables-")
         show_tables()
-        
+        print("\n")
+
         while conn.is_connected():
-            print("""\nDatabase Queries:-
-            1. Table Details
-            2. Record
-            3. Disconnect""")
-            action = input("\nEnter Command: ")
-            if action.upper() == "DISCONNECT":
+            print("""Database Queries:-
+            1. Describe a Table
+            2. Insert A Value
+            3. Close Connection
+            4. Select Data""")
+
+            action = input("(Enter Command) > ")
+            if action in ["Close Connection", "CLOSE CONNECTION", "3"]:
                 conn.close()
-            if action.upper() == "TABLE DETAILS":
-                describe_table()
-            if action.upper() == "RECORD DATA":
+            if action in ["Describe a Table", "DESCRIBE A TABLE", "1"]:
+                describe_table()  
+            if action in ["Insert A Value", "INSERT A VALUE", "2"]:
                 insert_values()
-                print("Values Recorded!\n")
-            if action.upper() == "":
-                pass #Continue Here
+            if action in ["Select Data", "SELECT DATA", "4"]:
+                pass
 
 except sq.Error:
-    print("\n✘ Connection Failed! ✘")
-    print(sq.Error)
-    print("Make sure you have entered the right credentials for the database connection")
+    print("\n[!] Connection Failed!")
+    print("[!] Make sure you have entered the right credentials for the database connection\n")
